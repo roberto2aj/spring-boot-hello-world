976b4d215d705cbbbf1cf75905dfe41fd633f9e5
Roberto Alves de Almeida Junior <roberto2aj@hotmail.com>
2020-06-07 14:55:08 -0300

    Pequenos ajustes

--- springboot.helloworld/src/main/java/br/com/roberto2aj/MainController.java
+++ springboot.helloworld/src/main/java/br/com/roberto2aj/MainController.java
@@ -1,20 +1,16 @@
 package br.com.roberto2aj;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class MainController {
 
-	@RequestMapping("/")
-	public List<String> helloWorld() {
-		List<String> helloWorld = new ArrayList<>();
-		helloWorld.addAll(Arrays.asList("Hello", "World", "!"));
-		return helloWorld;
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World!";
 	}
 
 }
